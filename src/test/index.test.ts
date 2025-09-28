354c27862164507e66f295fb080facac710e2d3f
François ROLAND <francois.roland@heh.be>
2025-09-28 16:46:56 +0200

    Import Gianni's tests
    Added unit tests for donnerAppreciation function to evaluate grades.

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,25 @@
 // Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
 
-import {...} from '../index';
+import {donnerAppreciation} from '../index';
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
+describe('Donner une appréciation d’une note\n', () => {
+    test('Très faible" si la note est entre 0 et 7', () => {
+        expect(donnerAppreciation(5)).toEqual('Très faible');
+    });
+    
+    test('"Moyenne" si la note est entre 8 et 12', () => {
+        expect(donnerAppreciation(10)).toEqual('Moyenne');
+    });
+    
+    test('"Bien" si la note est entre 13 et 16', () => {
+        expect(donnerAppreciation(15)).toEqual('Bien');
+    });
+    
+    test('"Excellent" si la note est entre 17 et 20', () => {
+        expect(donnerAppreciation(18)).toEqual('Excellent');
+    });
+    
+    test('Note invalide" si la valeur saisie est en dehors de [0, 20].', () => {
+        expect(donnerAppreciation(25)).toEqual('Note invalide');
     });
 });
